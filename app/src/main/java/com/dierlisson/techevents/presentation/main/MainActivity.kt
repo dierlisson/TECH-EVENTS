12889a3106ded2eb1126acd39cf43cd45c8a9ead
Dierlisson Justiniano <dierlisson1@gmail.com>
2026-07-30 19:52:35 -0300

    fix: resolve AppCompat theme inheritance for MainActivity

--- app/src/main/java/com/dierlisson/techevents/presentation/main/MainActivity.kt
+++ app/src/main/java/com/dierlisson/techevents/presentation/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.dierlisson.techevents.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import com.dierlisson.techevents.R
 import com.dierlisson.techevents.databinding.ActivityMainBinding
@@ -12,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
